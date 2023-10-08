6757208e9b2480cb02ca0f11289086a428f5863e
Sameh1Tarek <Sameh1Tarek>
2023-10-08 17:55:32 +0300

    add Shopping Cart Items CRUD

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -12,9 +12,11 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 
 const routes: Routes = [
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent},
@@ -31,7 +33,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -73,4 +73,24 @@ export class CartService {
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}`);
     console.log('----')
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if(theCartItem.quantity === 0){
+      this.remove(theCartItem);
+    }else{
+      this.computeCartTotals();
+    }
+  }
+
+
+  remove(theCartItem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
 }
